d94c9e62d8c9b898922d24574b5d6f660d1bb6fd
Vitor Hugo <vitorhugo.snp@gmail.com>
2023-08-15 14:00:16 -0400

    Responsive mobile navbar menu

--- java/script.js
+++ java/script.js
@@ -1,10 +1,47 @@
-const links = document.querySelectorAll('.navbar a');
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu)
+    this.navList = document.querySelector(navList)
+    this.navLinks = document.querySelectorAll(navLinks)
+    this.showClass = "show"
 
-for (const link of links) {
-  link.addEventListener('click', function (event) {
-    for (const otherLink of links) {
-      otherLink.classList.remove('active');
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = ``)
+        : (link.style.animation = `navLinkFade .5s ease forwards ${index / 7 + 0.3}s`)
+    })
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.showClass)
+    this.mobileMenu.classList.toggle(this.showClass)
+    this.animateLinks()
+  }
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick)
+  }
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent()
     }
-    this.classList.add('active');
-  });
+    return this
+  }
 }
+
+const mobileNavbar = new MobileNavbar(".mobile-menu", ".navbar", ".navbar li")
+mobileNavbar.init()
+
+const links = document.querySelectorAll(".navbar a")
+
+links.forEach(link => {
+  link.addEventListener("click", function (event) {
+    links.forEach(otherLinks => {
+      otherLinks.classList.remove("active")
+    })
+    this.classList.add("active")
+  })
+})

742c181f4b41fde7ce368987e55f4d7803e916cb
Vitor Hugo <vitorhugo.snp@gmail.com>
2023-08-15 17:26:25 -0400

    small changes

--- java/sectionJump.js
+++ java/sectionJump.js
@@ -1,13 +1,13 @@
 const sections = document.querySelectorAll("section:not(#header)")
 
 const options = {
-  threshold: 0.25 // Trigger when 25% of section is in view
+  threshold: 0.25
 }
 
 const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     const section = entry.target
-    const elementsToAnimate = section.querySelectorAll("h1, h2, p, ul, li, button, div:not(.container)") // Add more selectors as needed
+    const elementsToAnimate = section.querySelectorAll("h1, h2, p, ul, li, button, div:not(.container)")
 
     if (entry.isIntersecting) {
       elementsToAnimate.forEach(element => {
